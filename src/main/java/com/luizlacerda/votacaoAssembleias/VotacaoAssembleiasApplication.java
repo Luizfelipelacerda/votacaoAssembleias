a3038775eabac6e9f78ecadcf96c612bd9122ea3
luiz lacerda <luizfelipe_lacerda@ymail.com>
2024-11-19 18:30:47 -0300

    first commit

---
+++ src/main/java/com/luizlacerda/votacaoAssembleias/VotacaoAssembleiasApplication.java
@@ -0,0 +1,13 @@
+package com.luizlacerda.votacaoAssembleias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaoAssembleiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacaoAssembleiasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizlacerda/votacaoAssembleias/VotacaoAssembleiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizlacerda.votacaoAssembleias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacaoAssembleiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
